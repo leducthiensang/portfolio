82f92b3ed0cd58891fd431109db321921d771e24
Le Duc Thien Sang <leducthiensang@github.com>
2026-06-06 01:06:04 +0700

    Remove scroll indicator mouse animation from Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,33 +23,6 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
-      <div className='absolute bottom-10 w-full flex justify-center items-center'>
-        <a 
-          href='#about'
-          onClick={(e) => {
-            e.preventDefault();
-            const target = document.getElementById("about");
-            if (target) {
-              target.scrollIntoView({ behavior: "smooth" });
-            }
-          }}
-        >
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 cursor-pointer'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
     </section>
   );
 };
