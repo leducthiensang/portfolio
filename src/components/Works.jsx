95d4874e89fa5bf478b3c5b3fbcf123cdcc06778
Le Duc Thien Sang <leducthiensang@github.com>
2026-06-06 00:31:53 +0700

    Remove GitHub icon overlays from project cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
-import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
@@ -38,19 +37,6 @@ const ProjectCard = ({
               alt="project preview"
               className="w-full h-full object-cover rounded-2xl"
             />
-
-            <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-              <div
-                onClick={() => window.open(source_code_link, "_blank")}
-                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-              >
-                <img
-                  src={github}
-                  alt="source code"
-                  className="w-1/2 h-1/2 object-contain"
-                />
-              </div>
-            </div>
           </div>
 
           <div className="mt-5">
